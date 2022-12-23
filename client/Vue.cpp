ec33cd3b0809682357e170fd30949a2718388e98
Vad3L <vandrepoleo@gmail.com>
2022-12-23 20:23:33 +0100

    board border

--- client/Vue.cpp
+++ client/Vue.cpp
@@ -1,6 +1,6 @@
 #include "Vue.hpp"
 
-Vue::Vue(gf::Vector2u SSize, int mycolor) : window("tempete sur le chec", SSize),
+Vue::Vue(gf::Vector2u SSize, int mycolor) : window("tempete sur l'échiquier", SSize),
 						renderer(window),
 						myColor(mycolor) {
     ScreenSize = gf::Vector2u(1000, 1000);
@@ -25,12 +25,35 @@ void Vue::print(Plateau p) {
     
     renderer.setView(mainView);
 
-    gf::Color4f white = gf::Color::fromRgba32(240,217,181,255);
-    gf::Color4f black = gf::Color::fromRgba32(181,136,99,255);
+    gf::Color4f lightBrown = gf::Color::fromRgba32(240,217,181,255);
+    gf::Color4f darkBrown = gf::Color::fromRgba32(181,136,99,255);
 
     gf::RectangleShape shape({ sizeSquare, sizeSquare });
     shape.setAnchor(gf::Anchor::Center);
     
+    for(int i = 0 ; i < 10;++i){
+        shape.setPosition(gf::Vector2f(beginBoard.height - sizeSquare,(beginBoard.col - sizeSquare)+(i*sizeSquare)));
+        shape.setColor(gf::Color::fromRgba32(85,60,40));
+        renderer.draw(shape);
+    }
+    
+    for(int i = 0 ; i < 10;++i){
+        shape.setPosition(gf::Vector2f(beginBoard.height + (sizeSquare *8),(beginBoard.col - sizeSquare)+(i*sizeSquare)));
+        shape.setColor(gf::Color::fromRgba32(85,60,40));
+        renderer.draw(shape);
+    }
+
+    for(int i = 0 ; i < 8;++i){
+        shape.setPosition(gf::Vector2f(beginBoard.height + (i*sizeSquare),(beginBoard.col - sizeSquare)));
+        shape.setColor(gf::Color::fromRgba32(85,60,40));
+        renderer.draw(shape);
+    }
+
+    for(int i = 0 ; i < 8;++i){
+        shape.setPosition(gf::Vector2f(beginBoard.height + (i*sizeSquare),(beginBoard.col)+(8*sizeSquare)));
+        shape.setColor(gf::Color::fromRgba32(85,60,40));
+        renderer.draw(shape);
+    }
 
     //affichage plateau 
     for  (Case &c : p.state) {
@@ -40,17 +63,17 @@ void Vue::print(Plateau p) {
         shape.setPosition(gf::Vector2f(beginBoard.height + ((float)x * sizeSquare), beginBoard.col + ((float)y * sizeSquare)));
         if (y % 2 == 0) {
             if (x % 2 == 0) {
-                shape.setColor(white);
+                shape.setColor(lightBrown);
             }
             else {
-                shape.setColor(black);
+                shape.setColor(darkBrown);
             }
         } else {
             if (x % 2 == 0) {
-                shape.setColor(black);
+                shape.setColor(darkBrown);
             }
             else {
-                shape.setColor(white);
+                shape.setColor(lightBrown);
             }
         }
         renderer.draw(shape);
@@ -82,7 +105,8 @@ void Vue::print(Plateau p) {
         
         sprite.setTexture(sheet, gf::RectF::fromPositionSize({ (1.f / 6.f) * i, j }, { (1.f / 6.f), 0.5f }));
         sprite.setPosition(gf::Vector2f(beginBoard.height + ((float)x * sizeSquare) , beginBoard.col + ((float)y * sizeSquare)));
-        sprite.setScale(0.15625);
+        //sprite.setColor(gf::Color::Black);
+        sprite.setScale((1.f / 6.f));
         sprite.setAnchor(gf::Anchor::Center);
 		
         if (myColor == -1) {
