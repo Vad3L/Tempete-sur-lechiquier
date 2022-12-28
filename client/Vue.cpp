bb77eac6be184dcc8e28e24b5117178076f0a133
anthony-univ <gascaanthony0409@icloud.com>
2022-12-28 20:36:46 +0100

    change letter/number image in text

--- client/Vue.cpp
+++ client/Vue.cpp
@@ -2,7 +2,8 @@
 
 Vue::Vue(gf::Vector2u SSize, ChessColor mycolor) : window("Tempete sur l'échiquier", SSize),
 						renderer(window),
-						myColor(mycolor) {
+						myColor(mycolor),
+                        font("../data/DejaVuSans.ttf") {
 
     ScreenSize = gf::Vector2u(1300, 1000);
     sizeSquare = 64.0f;
@@ -21,7 +22,6 @@ Vue::Vue(gf::Vector2u SSize, ChessColor mycolor) : window("Tempete sur l'échiqu
     views.setInitialFramebufferSize(ScreenSize);
 
     sheetPiece = gf::Texture("../data/ChessSheet.png");
-    sheetNumberLetter = gf::Texture("../data/NumberLetterSheet.png");
 }
 
 void Vue::draw(Plateau p) {
@@ -33,38 +33,33 @@ void Vue::draw(Plateau p) {
     tableCloth.setColor(gf::Color::fromRgba32(85,60,40));
     tableCloth.setPosition(ScreenSize/2);
     renderer.draw(tableCloth);
+    
+    std::string tab[8] = {"A", "B", "C", "D", "E", "F", "G", "H"};
 
     //print border of board with letter and number
-    for(int i = 1 ; i <= 8;++i) {
-        gf::Sprite sprite;
-        // left numbers
-        sprite.setTexture(sheetNumberLetter, gf::RectF::fromPositionSize({ (1.f / 8.f) * (8-i), 0.f }, { (1.f / 8.f), 0.5f }));
-        sprite.setPosition(gf::Vector2f(beginBoard.col - sizeSquare/2,(beginBoard.height - sizeSquare/2)+(i*sizeSquare)));
-        sprite.setScale((1.f / 20.f));
-        sprite.setAnchor(gf::Anchor::Center);
-        renderer.draw(sprite);
+    for(int i = 1 ; i <= 8;++i){ 
+        // letter
+        float translation = (myColor == ChessColor::BLACK) ? 9*sizeSquare : 0;
+        gf::Text text(tab[i-1], font, 20);
+        text.setPosition(gf::Vector2f(beginBoard.col-sizeSquare/2 + (i*sizeSquare),(beginBoard.height)+(8.5*sizeSquare) - translation));
         
-        // right numbers
-        sprite.setTexture(sheetNumberLetter, gf::RectF::fromPositionSize({ (1.f / 8.f) * (i-1), 0.f }, { (1.f / 8.f), 0.5f }));
-        sprite.setPosition(gf::Vector2f(beginBoard.col+ (sizeSquare *8.5),(beginBoard.height - sizeSquare/2)+(i*sizeSquare)));
-        sprite.setRotation(gf::Pi);
-        renderer.draw(sprite);
-    }
+        if (myColor == ChessColor::BLACK) {
+            text.setRotation(gf::Pi);
+        }
+        
+        text.setAnchor(gf::Anchor::Center);
+        renderer.draw(text);
 
-    for(int i = 1 ; i <= 8;++i){
-        gf::Sprite sprite;
-        // bottom letters
-        sprite.setTexture(sheetNumberLetter, gf::RectF::fromPositionSize({ (1.f / 8.f) * (i-1), 0.5f }, { (1.f / 8.f), 0.5f }));
-        sprite.setPosition(gf::Vector2f(beginBoard.col-sizeSquare/2 + (i*sizeSquare),(beginBoard.height)+(8.5*sizeSquare)));
-        sprite.setScale((1.f / 22.f));
-        sprite.setAnchor(gf::Anchor::Center);
-        renderer.draw(sprite);
-
-        // top letters
-        sprite.setTexture(sheetNumberLetter, gf::RectF::fromPositionSize({ (1.f / 8.f) * (8-i), 0.5f }, { (1.f / 8.f), 0.5f }));
-        sprite.setPosition(gf::Vector2f(beginBoard.col-sizeSquare/2 + (i*sizeSquare),(beginBoard.height - sizeSquare/2)));
-        sprite.setRotation(gf::Pi);
-        renderer.draw(sprite);  
+        // number   
+        text.setString(std::to_string(9-i));
+        text.setPosition(gf::Vector2f(beginBoard.col - sizeSquare/2  + translation,(beginBoard.height - sizeSquare/2)+(i*sizeSquare)));
+        
+        if (myColor == ChessColor::BLACK) {
+            text.setRotation(gf::Pi);
+        }
+        
+        text.setAnchor(gf::Anchor::Center);
+        renderer.draw(text);
     }
 
     gf::Color4f lightBrown = gf::Color::fromRgba32(240,217,181,255);
@@ -72,8 +67,8 @@ void Vue::draw(Plateau p) {
 
     gf::RectangleShape shape({ sizeSquare, sizeSquare });
     //shape.setAnchor(gf::Anchor::Center);
-   
-    //affichage plateau 
+    
+    //draw plateau 
     for  (Case &c : p.state) {
         int x = c.position.x;
         int y = c.position.y;
@@ -95,15 +90,19 @@ void Vue::draw(Plateau p) {
             }
         }
 
+        // case selected
         if(y == p.caseSelected.y && x == p.caseSelected.x) {
             shape.setColor(gf::Color::Red);
         }
+
+        // move autorized
         for(auto &coord : p.moveAvailable) {
             if(y == coord.y && x == coord.x) {
                 shape.setColor(gf::Color::Gray(0.6));
                 break;
             }
         }
+
         shape.setAnchor(gf::Anchor::Center);
         shape.setOutlineColor(gf::Color::fromRgba32(85,60,40));
         shape.setOutlineThickness(2.5f);
@@ -117,7 +116,7 @@ void Vue::draw(Plateau p) {
         sprite.setTexture(sheetPiece, gf::RectF::fromPositionSize({ (1.f / 6.f) * i, j }, { (1.f / 6.f), 0.5f }));
         sprite.setPosition(gf::Vector2f(beginBoard.col+sizeSquare/2 + ((float)x * sizeSquare) , beginBoard.height+sizeSquare/2 + ((float)y * sizeSquare)));
         
-        sprite.setScale((1.f / 6.f));
+        sprite.setScale((1.f / 5.f));
         sprite.setAnchor(gf::Anchor::Center);
 		
         if (myColor == ChessColor::BLACK) {
