d09bd958a64472b3237487bfa108953f22ff4692
Trumilo <wall.clone@outlook.fr>
2023-01-04 14:51:53 +0100

    ajout des cartes dans le serveur

--- server/server.cpp
+++ server/server.cpp
@@ -1,4 +1,78 @@
 #include "AnnexFctServer.hpp"
+#include <cstring>
+class Carte{
+    private:
+        char name[20]; //Plus grand nom 15
+        bool beforeTurn;
+        bool afterTurn;
+        bool replaceTurn;
+        bool cancelMove;
+        bool cancelCarte;
+
+    public:
+        Carte(char *name,bool before, bool after, bool replace, bool cancelMove, bool cancelCarte);
+        //Getters
+        char getTurn(); //return when we can use the card b for beforeTurn, a for after
+        char* getEffect();//return return r for replace,cm for CancelMove and cc for CancelCarte
+        char* getName(); // return the name of the card
+
+        bool operator<(const Carte& rhs) const{
+            
+
+        }
+
+};
+
+
+
+Carte::Carte(char *name,bool before, bool after, bool replace, bool cancelMove, bool cancelCarte){
+    strcpy(this->name,name);
+    if(before && !after){
+        this->beforeTurn=true;
+    }
+    if(after && !before){
+        this->afterTurn=true;
+    }
+    if(replace && !after && !before){
+        this->replaceTurn=true;
+    }
+    if(cancelMove && !cancelCarte){
+        this->cancelMove=true;
+    }
+    if(cancelCarte && !cancelMove){
+        this->cancelCarte=true;
+    }
+}
+
+char Carte::getTurn(){
+    if(this->beforeTurn){
+        return 'b';
+    }
+    if(this->afterTurn){
+        return 'a';
+    }
+    return 'd'; //for during
+}
+
+char* Carte::getEffect(){
+    if(this->replaceTurn){
+        return (char *)"r";
+    }
+    if(this->cancelCarte){
+        return (char *)"cc";
+    }
+    if(this->cancelMove){
+        return (char *)"cm";
+    }
+
+    return (char *)"n"; //for none
+}
+
+char* Carte::getName(){
+    return this->name;
+}
+
+
 
 int main (int argc, char* argv[]) {
 	if (argc != 2) {
@@ -41,6 +115,35 @@ int main (int argc, char* argv[]) {
             
             Plateau plateau;
             bool turnPlayer1 = true;
+
+            //Création des cartes
+            std::set<Carte> deck;
+            Carte c1((char *)"Chameau",false,true,false,false,false);
+            Carte c2((char *)"Coup d'etat",false,true,false,false,false);
+            Carte c3((char *)"Pas question !",true,false,false,false,true);
+            Carte c4((char *)"Niet.",true,false,false,false,true);
+            Carte c5((char *)"Charge",false,false,true,false,false);
+            Carte c6((char *)"Banzai",false,false,true,false,false);
+            Carte c7((char *)"Amphetamines",false,true,false,false,false);
+            Carte c8((char *)"Favorite",false,false,true,false,false);
+            Carte c9((char *)"Bombe Atomique",false,true,false,false,false);
+            Carte c10((char *)"Trappe",false,true,false,false,false);
+            Carte c11((char *)"Quatre Coins",false,false,true,false,false);
+            Carte c12((char *)"Exil",false,true,false,false,false);
+
+            deck.insert(c1);
+            deck.insert(c2);
+            deck.insert(c3);
+            deck.insert(c4);
+            deck.insert(c5);
+            deck.insert(c6);
+            deck.insert(c7);
+            deck.insert(c8);
+            deck.insert(c9);
+            deck.insert(c10);
+            deck.insert(c11);
+            deck.insert(c12);
+
 
             std::cout << "Je suis le serveur" << std::endl;
             while (true) {
