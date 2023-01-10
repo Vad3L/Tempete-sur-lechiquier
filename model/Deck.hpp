936c80700a6faf97876f6cfb7fab42f34cdf5983
Trumilo <wall.clone@outlook.fr>
2023-01-10 23:27:02 +0100

    Ajout des cartes et du deck

---
+++ model/Deck.hpp
@@ -0,0 +1,27 @@
+#ifndef DECK_H
+#define DECK_H
+
+#include <set>
+
+#include "Card.hpp"
+
+
+
+class Deck{
+    public:
+        Deck();
+        void displayDeck() const;
+    private:
+        std::vector<Card> m_deck;
+        std::vector<Card> m_discard;
+
+    private:
+        void swap(Card &a, Card &b);
+        void shuffle();
+        Card getFirst();
+        void drop(Card m_card);
+
+
+};
+
+#endif // DECK_H
