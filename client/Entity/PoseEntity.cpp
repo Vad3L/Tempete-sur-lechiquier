f5db5388cb5943523ab65bf8fb1f11f2c4c92094
Trumilo <wall.clone@outlook.fr>
2023-01-16 12:59:58 +0100

    Finalisation de la zone de pose

--- client/Entity/PoseEntity.cpp
+++ client/Entity/PoseEntity.cpp
@@ -4,6 +4,7 @@ PoseEntity::PoseEntity(gf::ResourceManager& resources,GameData &gameData)
 : m_cardsIllustration(resources.getTexture("images/CardIlustrationSheet.png"))
 , m_accessories(resources.getTexture("images/AccesoriesCards.png"))
 , m_gameData(gameData)
+, m_poseNameFont(resources.getFont("fonts/DejaVuSans.ttf"))
 {
 
 }
@@ -16,7 +17,7 @@ void PoseEntity::render(gf::RenderTarget &target, const gf::RenderStates &states
     gf::Coordinates coords({500,500});
     
     gf::Vector2i sizeCard = gf::Vector2i(200,300);
-    gf::Vector2f position = coords.getRelativePoint({ -0.4f, -0.25f });
+    gf::Vector2f position = coords.getRelativePoint({ -1.f, -0.25f });
     
     gf::RoundedRectangleShape contour(sizeCard+10.f);
     contour.setColor(gf::Color::fromRgba32(0.f, 0.f, 0.f, 0.f));
@@ -25,5 +26,20 @@ void PoseEntity::render(gf::RenderTarget &target, const gf::RenderStates &states
     contour.setRadius(22);
     contour.setPosition(position);
 
+    gf::RectangleShape placementTexte;
+    placementTexte.setSize({90.0f,1.0f});
+    placementTexte.setColor(gf::Color::Black);
+    placementTexte.setOutlineThickness(6.f);
+    placementTexte.setOutlineColor(gf::Color::Black);
+    placementTexte.setPosition({position.x+110.f,position.y-4.f});
+    placementTexte.setAnchor(gf::Anchor::Center);
+
+    gf::Text poseName("Carte joué", m_poseNameFont, 16.f);
+    poseName.setColor(gf::Color::White);
+    poseName.setPosition({position.x+110.f,position.y-3.f});
+    poseName.setAnchor(gf::Anchor::Center);
+
     target.draw(contour, states);
+    target.draw(placementTexte,states);
+    target.draw(poseName,states);
 }
