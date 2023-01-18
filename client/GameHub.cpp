c68d03a7f6eb5adfd4aef2f01b670bd7eed7775d
anthony-univ <gascaanthony0409@icloud.com>
2023-01-18 22:22:37 +0100

    push commentaire debug

--- client/GameHub.cpp
+++ client/GameHub.cpp
@@ -7,7 +7,7 @@ GameHub::GameHub()
 {
     getWindow().setResizable(false);  
     getWindow().setSize(gf::Vector2i(640,420));
-    getWindow().setFullscreen();
+    //getWindow().setFullscreen();
 }
 
 void GameHub::loadingDownloadAssets() {
@@ -15,12 +15,20 @@ void GameHub::loadingDownloadAssets() {
 }
 
 void GameHub::loadingAssets() {
+    gf::Log::info("fin debut chargment\n");
     start = std::make_unique<StartScene>(*this);
+    gf::Log::info("fin chargment start scene\n");
     menu = std::make_unique<MenuScene>(*this);
+    gf::Log::info("fin chargement menu scene\n");
     rules = std::make_unique<RulesScene>(*this);
+    gf::Log::info("fin chargement rules scene\n");
     play = std::make_unique<PlaySelectScene>(*this);
+    gf::Log::info("fin chargement play scene\n");
     game = std::make_unique<GameScene>(*this);
+    gf::Log::info("fin chargment game scene\n");
     quit = std::make_unique<QuitScene>(*this);
+    gf::Log::info("fin chargment quit scene\n");
+    gf::Log::info("fin chargment total\n");
     
     m_loadingFinish = true;
 }
